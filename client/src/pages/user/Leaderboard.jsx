22208dbf498fc9da7e8a830901e9470111bfa083
ShamilAliCholayil <shamilalicholayil@gmail.com>
2026-08-10 17:23:23 +0530

    Fix leaderboard not refetching when track filter changes

--- client/src/pages/user/Leaderboard.jsx
+++ client/src/pages/user/Leaderboard.jsx
@@ -91,7 +91,7 @@ export default function Leaderboard() {
 
     loadLeaderboard();
     loadMyRank();
-  }, [difficulty]);
+  }, [track, difficulty]);
 
   return (
     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
